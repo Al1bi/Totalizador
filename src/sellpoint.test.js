4882fde08f778b52458e7bf254467b73d43c79a9
Al1bi <denilson.fiesta@ucb.edu.bo>
2023-08-24 16:35:04 -0400

    funcionalidad para el precio total del impuesto segun el precio neto y el porcentaje de TC verificado

--- src/sellpoint.test.js
+++ src/sellpoint.test.js
@@ -1,22 +1,27 @@
 import SellPoint from "./sellpoint";
 describe("SellPoint", () => {
-    it("deberia devolver la entrada de usuario", () => {
-        const sp = new SellPoint('TX', 1,1)
-        expect(sp.getPrecioNeto()).toEqual(1);
-    });
+  it("deberia devolver la entrada de usuario", () => {
+    const sp = new SellPoint('TX', 1,1)
+    expect(sp.getPrecioNeto()).toEqual(1);
   });
+});
 describe("SellPoint", () => {
-    it("deberia devolver la entrada de usuario", () => {
-        const sp = new SellPoint('TX', 2,1)
-        expect(sp.getPrecioNeto()).toEqual(2);
-
-    });
+  it("deberia devolver la entrada de usuario", () => {
+    const sp = new SellPoint('TX', 2,1)
+    expect(sp.getPrecioNeto()).toEqual(2);
   });
+});
 
 describe("SellPoint", () => {
-    it("Sea Cantidad 5, Precio 2, Estad CA deberia retornar 0.0825", () => {
-        const sp = new SellPoint('CA', 5,2)
-        expect(sp.getVerificarImpuesto(0.825)).toEqual(true);
-
-    });
+  it("Sea Cantidad 5, Precio 2, Estad CA deberia retornar 0.0825", () => {
+    const sp = new SellPoint('CA', 5,2)
+    expect(sp.getVerificarImpuesto(0.825)).toEqual(true);
   });
+});
+
+describe("SellPoint", () => {
+  it("Sea Cantidad 20, Precio 100, Estad CA deberia retornar 125", () => {
+    const sp = new SellPoint('TX', 20, 100)
+    expect(sp.getVerificarImpuesto(125)).toEqual(true);
+  });
+})
